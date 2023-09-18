f934afbad2847463a29f933befa7cbb129e8883e
Lucas Loz <lucas.loz@cebraspe.org.br>
2023-09-18 10:50:31 -0300

    build: install one-signal

--- App.tsx
+++ App.tsx
@@ -1,13 +1,20 @@
-import { StatusBar } from 'react-native';
-import { NativeBaseProvider } from 'native-base';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { StatusBar } from "react-native";
+import { NativeBaseProvider } from "native-base";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 
-import { Routes } from './src/routes';
+import { Routes } from "./src/routes";
 
-import { THEME } from './src/theme';
-import { Loading } from './src/components/Loading';
+import { THEME } from "./src/theme";
+import { Loading } from "./src/components/Loading";
 
-import { CartContextProvider } from './src/contexts/CartContext';
+import { CartContextProvider } from "./src/contexts/CartContext";
+import { OneSignal } from "react-native-onesignal";
+
+OneSignal.initialize("7831375e-5b84-40fa-a178-17c71c78759e");
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
